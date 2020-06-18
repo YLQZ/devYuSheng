06f0563d1cf128b46dae6117bea788dcec28a85e
1194979849@qq.com <1194979849@qq.com>
2020-06-18 19:22:15 +0800

    up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       user: 'git', // SSH 端口
       host: '39.96.24.186', // SSH 配置
       ref: 'origin/master', // GIT 仓库
-      repo: 'git@github.com:YLQZ/devYuSheng.git', // 服务器部署路径
+      repo: 'https://github.com/YLQZ/devYuSheng.git', // 服务器部署路径
       path: '/home/www/devyusheng', // 安装后置任务
       'post-setup': 'npm install', // 部署后置任务
       'post-deploy': 'npm run build'
