ccc15a947b43cc7e76a5be9ef277e8a286d0afa4
1194979849@qq.com <1194979849@qq.com>
2020-06-18 18:43:57 +0800

    up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-
   deploy: {
     // "production" 是环境名称
     production: {
       // SSH key 路径
+      key: `${process.env.HOME}/.ssh/id_rsa.pub`,
       user: 'git', // SSH 端口
-      host: ['39.96.24.186'], // SSH 配置
-      port: 22,                // ssh端口
-      ssh_options: 'StrictHostKeyChecking=no', // GIT 远程分支
+      host: '39.96.24.186', // SSH 配置
       ref: 'origin/master', // GIT 仓库
       repo: 'git@github.com:YLQZ/devYuSheng.git', // 服务器部署路径
-      path: '/home/project/repository', // 安装后置任务
+      path: '/home/www/devyusheng', // 安装后置任务
       'post-setup': 'npm install', // 部署后置任务
       'post-deploy': 'npm run build'
     }
