43c6e31195275ea30917af97286613fcd8d1df6f
1194979849@qq.com <1194979849@qq.com>
2020-06-19 21:40:17 +0800

    uup

---
+++ pm2.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  deploy: {
+    production: {
+      user: 'root',
+      host: '39.96.24.186',
+      ref: 'origin/master',
+      repo: 'https://github.com/YLQZ/devYuSheng.git',
+      path: '/home/www/devyusheng',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': "echo 'Deploy Done!' && npm install && npm run build",
+      // 'post-deploy':
+      //   'npm install && pm2 reload ecosystem.config.js --env production',
+      // 'pre-setup': ''
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
